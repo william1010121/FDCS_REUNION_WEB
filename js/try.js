e6fbe5761902b9638e4d7c18350e33236c049ed8
郭勝威 <william950615@gmail.com>
2022-11-16 21:51:41 +0800

    origin(without crypt)

--- js/try.js
+++ js/try.js
@@ -1,8 +1,43 @@
 var DATA;
 var MUSIC;
-var FatherFileNmae = "FDCS_REUNION_WEB/";
-// var FatherFileNmae = "";
+// var FatherFileNmae = "FDCS_REUNION_WEB/";
+var FatherFileNmae = "";
 var MAIN;
+var Cookie;
+
+
+class cookie {
+    setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toString();
+        document.cookie = cname + "=" + cvalue + "; " + expires;
+    }
+    getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i].trim();
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+    LoginTimeCnt() {
+        var cookie = this.getCookie("time_cnt");
+        var cookieval = Number(cookie);
+        if (cookie == "") {
+            this.setCookie("time_cnt", 1, 30);
+            cookieval = 1;
+        }
+        else {
+            this.setCookie("time_cnt", Number(cookie) + 1, 30);
+            cookieval = Number(cookieval) + 1;
+        }
+        document.getElementById("login time").innerText = "\u9019\u662F\u4F60\u7B2C".concat(Number(cookieval) + 1, "\u6B21\u4F86\u5230\u9019\u500B\u7DB2\u7AD9");
+    }
+}
 
 class GLOBAL_DATA{
     constructor(){
@@ -10,6 +45,7 @@ class GLOBAL_DATA{
             "red", "green", "yellow", "black", "whilte"
         ];
         this.BgcolorCnt = 0;
+        this.EndString = "謎題開始";
     }
 
     ChangeBgColor(element) {
@@ -17,6 +53,10 @@ class GLOBAL_DATA{
         this.BgcolorCnt = (this.BgcolorCnt+1)%sz;
         console.log(sz);
         element.Setstyle('element-color', this.BGCOLOR[this.BgcolorCnt]);
+    }
+
+    Encropy(value) {
+        return value;
     }
 }
 
@@ -62,6 +102,11 @@ class Element {
         this.StylePositoin = position;
         this.ElementCnt = 0;
         this.PrevEle = "none";
+        
+        this.Password = "none";
+        this.PasswordId = "none";
+        this.Status = "Go";
+        this.ReadPassword();
     }
 
     SetAttr(tag, value) {
@@ -92,18 +137,24 @@ class Element {
     }
 
 
-    AddElement(tag='p') {
+    AddElement(tag='p', InHtml='', class_='', RegardAsPreEle = true) {
+        if( this.Status == 'stop' ) {
+            console.log("Now pause");
+            return "false";
+        }
         this.ElementCnt++;
-        this.ele.innerHTML += `<${tag} id="${this.ElementCnt}th element of ${this.name}"></${tag}>`
-        this.PrevEle =  `${this.ElementCnt}th element of ${this.name}`;
+        this.ele.innerHTML += `<${tag} id="${this.ElementCnt}th element of ${this.name}" class="${class_}">${InHtml}</${tag}>`
+        if( RegardAsPreEle)
+            this.PrevEle =  `${this.ElementCnt}th element of ${this.name}`;
         return `${this.ElementCnt}th element of ${this.name}`;
     }
 
-    EndString(st='故事結束') {
+
+    EndString() {
         const ele = document.getElementById(this.PrevEle);
         if( ele == null ) return false;
         // console.log(ele.innerText);
-        if( ele.innerText == st ) {
+        if( ele.innerText == DATA.EndString ) {
             return true;
         }
         else {
@@ -111,8 +162,34 @@ class Element {
         }
     }
 
+    NewStory() {
+        this.CleanElement();
+        this.Status = "Go";
+    }
+
     CleanElement() {
         this.ele.innerHTML = "";
+    }
+    AddPassword(PwdId) {
+        this.PasswordId = PwdId;
+        this.Status = 'stop';
+    }
+    GetPassword() {
+        if( this.PasswordId == 'none') {
+            console.log("No password Get");
+            return "No password Get";
+        }
+        let InputPsd = document.getElementById(this.PasswordId).children[0];        
+        console.log("Your Guest password is", InputPsd.value);
+
+        return InputPsd.value;
+    }
+    CheckPassword() {
+        return this.Password == this.GetPassword();
+    }
+
+    ReadPassword() {
+        this.Password = "123";
     }
 }
 
@@ -141,7 +218,7 @@ class Liter {
         this.story.then((val) => {
             // console.log(val);
             if( this.NowIdx == val.length ) {
-                document.getElementById(id).innerHTML += "故事結束";
+                document.getElementById(id).innerHTML += DATA.EndString;
                 return;
             }
             console.log(val[ this.NowIdx])
@@ -150,6 +227,7 @@ class Liter {
         }
         );
     }
+
 }
 
 
@@ -159,9 +237,11 @@ function init() {
     MUSIC = new music();    
     DATA = new GLOBAL_DATA();
     MAIN = new Element(document.getElementById('main_process'), 'main_process', "");
+    Cookie = new cookie();
 
     let Body = new Element(document.getElementById("BODY"), "body", `../${FatherFileNmae}style/StartPage.json`);
     Body.ReadStyle();
+    Cookie.LoginTimeCnt();
 
 
     let story = new Liter(`../${FatherFileNmae}data/story/start.json`);
@@ -171,7 +251,7 @@ function init() {
 
 
     document.addEventListener('keydown', function(event){
-        // console.log(event.key);
+        //console.log(event.key);
         if( event.key == 'p') {
             MUSIC.Change()
         }
@@ -179,10 +259,26 @@ function init() {
             MAIN.ScroolToEnd();
             // console.log(MAIN.EndString());
             if( MAIN.EndString() ) {
-                story.ReadWord();
+                console.log(MAIN.Status);
+                if( MAIN.Status == "Go")  {
+                    MAIN.AddPassword(MAIN.AddElement('div', '<input/>', 'center', false));
+                    MAIN.status = "stop";
+                }
             }
-            story.NextandAddInto(MAIN.AddElement());
+            else {
+                story.NextandAddInto(MAIN.AddElement());
+            }
+        }
+        if( event.key == '/' ) {
+            let ans = MAIN.CheckPassword();
+            if( ans ) {
+                story.ReadWord();
+                MAIN.NewStory();
+                alert("密碼正確");
+            }
+            else {
+                alert("密碼不正確");
+            }
         }
     });
-
 }
